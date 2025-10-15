8454fd924b0fa735529b144e6eab8bd94d7a316c
Gizem Meral <gizemdataprojects@gmail.com>
2025-10-15 10:06:17 +0300

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -8,7 +8,7 @@ Script Purpose:
     within the database: 'bronze', 'silver', and 'gold'.
 	
 WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    Running this script will drop the entire 'Supply_Chain_Datawarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
